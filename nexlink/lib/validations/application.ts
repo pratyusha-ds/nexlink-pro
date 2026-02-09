c473d48ff1e5716e36555568a1a134a38e000d3c
Pratyusha <167186378+pratyusha-ds@users.noreply.github.com>
2026-02-09 18:32:19 +0100

    Update Zod schema to match the Prisma schema

--- nexlink/lib/validations/application.ts
+++ nexlink/lib/validations/application.ts
@@ -1,32 +1,29 @@
 import { z } from "zod";
 
+const ApplicationStatus = z.enum([
+  "PENDING",
+  "APPLIED",
+  "BEING_PROCESSED",
+  "WAITING_FOR_INTERVIEW",
+  "REJECTED",
+]);
+
+const ApplicationType = z.enum(["REMOTE", "ON_SITE", "HYBRID"]);
+
 export const applicationSchema = z.object({
-  //Job Details for the company applied to
-  companyName: z.string({ error: "Company name is required" }).min(1),
-  website: z.url({ error: "Please enter a valid URL" }).or(z.literal("")),
-  logoUrl: z.string().optional(),
-  isManual: z.boolean().default(false),
-  jobTitle: z
-    .string({ error: "Job title must be at least 2 characters" })
-    .min(2),
-  jobUrl: z
-    .url({ error: "Please enter a valid job posting link" })
-    .or(z.literal("")),
-  location: z.string({ error: "Location is required" }).min(1),
-  salary: z.string().optional(),
-  dateApplied: z.date({ error: "Please select a date" }),
-
-  //Documents like CV, Cover Letter, etc
-  cv: z.custom<FileList>().optional(),
-  extraDocs: z
-    .custom<FileList>()
-    .optional()
-    .refine((f) => !f || f.length <= 5, { error: "Max 5 documents" }),
-
-  //Status
-  status: z
-    .enum(["Saved", "Applied", "Interviewing", "Offer", "Rejected"])
-    .default("Applied"),
+  companyName: z.string().min(1, "Company name is required"),
+  jobTitle: z.string().min(2, "Job title must be at least 2 characters"),
+  status: ApplicationStatus,
+  type: ApplicationType,
+  website: z.url("Invalid URL").or(z.literal("")).nullable(),
+  jobUrl: z.url("Invalid job link").or(z.literal("")).nullable(),
+  logoUrl: z.string().nullable().optional(),
+  description: z.string().min(1, "Description is required"),
+  email: z.email("Invalid email address"),
+  location: z.string().min(1, "Location is required"),
+  notes: z.string().nullable().optional(),
+  salary: z.string().nullable().optional(),
+  interviewDnT: z.date().nullable().optional(),
 });
 
 export type ApplicationFormValues = z.infer<typeof applicationSchema>;
