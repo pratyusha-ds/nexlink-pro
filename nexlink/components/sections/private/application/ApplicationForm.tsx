5573993be5eaf62cf6d2abd1f640260fab6b2162
Pratyusha <167186378+pratyusha-ds@users.noreply.github.com>
2026-02-09 23:03:54 +0100

    Create basic Application Form

---
+++ nexlink/components/sections/private/application/ApplicationForm.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form } from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+
+import {
+  applicationSchema,
+  type ApplicationFormValues,
+} from "@/lib/validations/application";
+
+export default function ApplicationForm() {
+  const form = useForm<ApplicationFormValues>({
+    resolver: zodResolver(applicationSchema),
+    defaultValues: {
+      companyName: "",
+      jobTitle: "",
+      location: "",
+      description: "",
+      email: "",
+      status: "PENDING",
+      type: "REMOTE",
+    },
+  });
+
+  function onSubmit(values: ApplicationFormValues) {
+    console.log(values);
+  }
+
+  return (
+    <div className="max-w-3xl mx-auto p-6 bg-white rounded-xl shadow-sm border border-slate-100">
+      <h2 className="text-2xl font-bold text-slate-800 mb-6">
+        Application Form
+      </h2>
+
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          {/* <CompanySection form={form} /> */}
+          {/* <StatusSection form={form} /> */}
+          {/* <DocumentSection form={form} /> */}
+
+          <Button
+            variant="default"
+            className="bg-green-700 hover:bg-green-800 text-white w-full py-6"
+          >
+            Save Application
+          </Button>
+        </form>
+      </Form>
+    </div>
+  );
+}
