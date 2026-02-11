9c5f7a7607c61f31b7c7b9baee1ea4e76c8e6da7
Emmanuel Ungab <john.ungab7@gmail.com>
2026-02-11 23:45:35 +0800

    fix: header, sidebar, and spacing on private pages

--- nexlink/app/(user)/create/page.tsx
+++ nexlink/app/(user)/create/page.tsx
@@ -1,14 +1,13 @@
 import ApplicationForm from "@/components/sections/private/application/ApplicationForm";
 
+// TODO: improve ui/ux; have the same padding per page
 export default function CreateApplicationPage() {
   return (
-    <div className="flex-1 w-full min-h-svh py-10 px-6 flex flex-col items-center justify-center">
-      <div className="w-full max-w-4xl mx-auto">
-        <div className="mb-10 space-y-2 text-center">
-          <h1 className="text-4xl font-extrabold tracking-tight">
-            Add New Application
-          </h1>
-          <p className="text-lg text-muted-foreground">
+    <div className="flex-1 w-full min-h-svh py-10 pt-24 md:pt-10 px-6 flex flex-col items-center justify-center">
+      <div className="w-full mx-auto">
+        <div className="space-y-2 mb-4">
+          <h1 className="text-xl md:text-3xl font-bold">Add New Application</h1>
+          <p className="text-sm md:text-base text-muted-foreground">
             Fill in the details manually or search for a company to auto-fill.
           </p>
         </div>
--- nexlink/components/header/UserHeader.tsx
+++ nexlink/components/header/UserHeader.tsx
@@ -4,16 +4,12 @@ import { useSession } from "@clerk/nextjs";
 import { Skeleton } from "../ui/skeleton";
 import { ReactNode } from "react";
 
-const UserHeader = ({children} : {children: ReactNode}) => {
-      const { isLoaded } = useSession();
-  if (!isLoaded) 
-    return <Skeleton className="fixed h-16 w-full flex items-center justify-between px-6 z-10 top-0 drop-shadow-sm"></Skeleton>;
-    
+const UserHeader = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="bg-white fixed h-16 w-full flex items-center justify-between px-6 z-10 top-0 drop-shadow-sm">
+    <div className="bg-white fixed h-16 w-full flex items-center justify-between px-6 z-10 top-0 drop-shadow-sm md:hidden">
       {children}
     </div>
-  )
-}
+  );
+};
 
-export default UserHeader
+export default UserHeader;
