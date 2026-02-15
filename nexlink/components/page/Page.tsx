95c3ad27b6b7e97e3bcabeff46a29924b7c5e999
Emmanuel Ungab <john.ungab7@gmail.com>
2026-02-15 18:40:10 +0800

    update: application table list

---
+++ nexlink/components/page/Page.tsx
@@ -0,0 +1,8 @@
+import { ReactNode } from "react";
+
+// TODO: create a reusable page to have consistent padding and margin
+function Page({ children }: { children: ReactNode }) {
+  return <div></div>;
+}
+
+export default Page;
